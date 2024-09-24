fb312ee8d5ec909851a2c039a9d68e0961c54c9e
Frankd11366 <capurivas@gmail.com>
2024-09-23 20:40:24 -0600

    funcion para vista administrador

--- views/pages/menu.php
+++ views/pages/menu.php
@@ -1,11 +1,12 @@
 <h1>MENU PRINCIPAL DE LA APP</h1>
 <a href="/igc_final/logout" class="btn btn-danger"> SALIR</a>
+
+
+<?php if($_SESSION['user']['rol_nombre_ct'] == 'INSTITUTO_ADMIN') : ?>
 <P>USUARIO ADMINISTRADOR</P>
 
 
-<?php var_dump($_SESSION);
-
-?>
+<?php endif ?>
 
 
 
